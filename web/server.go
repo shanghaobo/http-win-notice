e193a66f50d4219c140ca34b87b2683aca3039fb
shanghaobo <1186156343@qq.com>
2023-08-24 14:27:50 +0800

    style

--- web/server.go
+++ web/server.go
@@ -25,15 +25,7 @@ func runServer() *http.Server {
 	}()
 
 	fmt.Println("Server started on :" + setting.PortStr())
-
-	//time.Sleep(10 * time.Second)
-	//ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	//defer cancel()
-	//if err := server.Shutdown(ctx); err != nil {
-	//	fmt.Println("server shutdown error")
-	//} else {
-	//	fmt.Println("server stop")
-	//}
+	
 	return server
 }
 
